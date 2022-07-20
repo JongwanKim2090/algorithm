8724316d5cdb128633c06a1200cf84119a7f8ce1
Jongwan Kim <jongwankim@snu.ac.kr>
2022-07-21 00:12:02 +0900

    commit

--- two pointer.py
+++ two pointer.py
@@ -11,10 +11,6 @@ count = 0
 sum = 0
 
 for start in range(len(data)):
-    # 스타트 하나씩  증가
-    # 인접한  합이  기준값보다 큰 지  아닌지 비교
-    # 맞으면  카운트 증가 하고  스타트  값  증가
-    # 아니면  end increase and next array value addtion
     while sum < m and end < len(data):
         sum+=data[end]
         end+=1
