5384a4d411ae4ec37eff6be839164fc38dc36679
Jongwan Kim <jongwankim@snu.ac.kr>
2022-07-22 00:32:44 +0900

    commit

---
+++ codeup/print_basic.py
@@ -0,0 +1,37 @@
+'''
+Hello
+World
+(두 줄에 걸쳐 줄을 바꿔 출력)
+'''
+
+print('Hello\nWorld')
+print('''Hello
+World''')
+
+
+
+'''
+'Hello'
+'''
+
+print("'Hello'")
+
+'''
+"Hello World"
+(단, 큰따옴표도 함께 출력한다.)
+'''
+
+print("\"Hello World\"")
+
+'''
+"!@#$%^&*()'
+(단, 큰따옴표와 작은따옴표도 함께 출력한다.)
+'''
+
+print('"!@#$%^&*()\'')
+
+'''
+"C:\Download\'hello'.py"
+'''
+print('"C:\Download\\\'hello\'.py"')
+
