8f586dbccebce1ca13e46e395b7fef79d3b9f4fb
Jongwan Kim <jongwankim@snu.ac.kr>
2022-07-25 00:45:44 +0900

    commit

---
+++ codeup/while input.py
@@ -0,0 +1,14 @@
+while True:
+    n = int(input())
+    if n != 0:
+        print(n)
+    elif n==0:
+        break
+
+while True:
+    a = input()
+    a = int(a)
+    if a == 0:
+        break
+    else:
+        print(a)
