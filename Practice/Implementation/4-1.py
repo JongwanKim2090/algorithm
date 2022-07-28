b1ad6262b6b63270b3f3bfaf4c1d8541b83a17c2
JongwanKim2090 <jongwankim@snu.ac.kr>
2022-07-29 02:26:14 +0900

    commit

--- Practice/Implementation/4-1.py
+++ Practice/Implementation/4-1.py
@@ -2,18 +2,20 @@ n = int(input())
 x, y = 1, 1
 plans = input().split()
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+steps = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+#dx = [0, 0, -1, 1]
+#dy = [-1, 1, 0, 0]
 move_type = ['L', 'R', 'U', 'D']
 
 for plan in plans:
     for i in range(len(move_type)):
         if plan == move_type[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = x + steps[i][0]
+            ny = y + steps[i][1]
     
     if nx<1 or nx>n or ny<1 or ny>n:
         continue
     x, y = nx, ny
 
 print(x, y)
+
